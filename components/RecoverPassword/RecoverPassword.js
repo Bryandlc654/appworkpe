bbefb8dea5533923d825a24dc56b6db19e70f95d
Bryandlc654 <bdelacruz654@gmail.com>
2024-05-15 22:24:41 -0500

    Update Splash Scrren

--- components/RecoverPassword/RecoverPassword.js
+++ components/RecoverPassword/RecoverPassword.js
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#ffffff',
-        padding: 20,
+        paddingHorizontal: 20,
         paddingTop: 140,
     },
     backButton: {
